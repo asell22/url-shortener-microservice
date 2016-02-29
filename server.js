5117ead95cb535690e3faeefeb2fa47b68157bb5
asell22 <alexanderjsell@gmail.com>
2016-02-29 18:56:25 +0000

    landing page and json response

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 var express = require('express');
 var app = express();
+var urlsObj = {};
+var count = 1;
 
 app.use(express.static('public'));
+
+app.get('/:url(*)', function(req, res) {
+   var requestUrl = req.hostname;
+   var url = req.params.url;
+   urlsObj[count] = url;
+   
+   res.send({
+       original_url: url,
+       short_url: requestUrl + "/" + count
+   });
+});
+
 
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("Server running on ", process.env.PORT);
