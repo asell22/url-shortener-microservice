e85fba223aa4d5e1b00868caa9a91d015f90da09
asell22 <alexanderjsell@gmail.com>
2016-02-29 20:10:20 +0000

    increment variable count middleware

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var app = express();
 var urlsObj = {};
-var count = 1;
+var count = 0;
 
 app.use(express.static('public'));
 
+// Middleware for checking to see if url is in urlsObj
+
+// Middleware to validate url
 var setRoutes = function(req, res, next) {
+    
     for (var num in urlsObj) {
         console.log(urlsObj[num]);
         app.get('/' + num, function(req, res) {
@@ -17,18 +21,28 @@ var setRoutes = function(req, res, next) {
 
 app.use(setRoutes);
 
-app.get('/new/:url(*)', function(req, res) {
-   var requestUrl = req.hostname;
+app.get('/new/:url(*)', function(req, res, next) {
+   var host = req.hostname;
    var url = req.params.url;
    urlsObj[count] = url;
    
+   app.use(setRoutes);
+   
     res.send({
        original_url: url,
-       short_url: requestUrl + "/" + count,
+       short_url: host + '/' + count,
        obj: urlsObj
     });
+    
+    next();
 });
 
+var incrementCount = function(req, res, next) {
+    count += 1;
+    next();
+}
+
+app.use(incrementCount);
 
 
 
