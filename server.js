20f10b15ac228aaf3178dc22de12e18f9658273e
asell22 <alexanderjsell@gmail.com>
2016-02-29 19:14:34 +0000

    shortened url redirects to original parameter

--- server.js
+++ server.js
@@ -5,16 +5,29 @@ var count = 1;
 
 app.use(express.static('public'));
 
-app.get('/:url(*)', function(req, res) {
+
+
+app.get('/new/:url(*)', function(req, res) {
    var requestUrl = req.hostname;
    var url = req.params.url;
+   count += "";
    urlsObj[count] = url;
+   
+   for (var num in urlsObj) {
+        console.log(urlsObj[num]);
+    app.get('/' + num, function(req, res) {
+        res.redirect(urlsObj[num]);
+    });
+}
    
    res.send({
        original_url: url,
-       short_url: requestUrl + "/" + count
+       short_url: requestUrl + "/" + count,
+       obj: urlsObj
    });
 });
+
+
 
 
 app.listen(process.env.PORT, process.env.IP, function() {
