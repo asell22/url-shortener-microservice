a21b52e5d9b54b9a32dc08e1460dc70c3a1d07d2
asell22 <alexanderjsell@gmail.com>
2016-03-01 04:52:34 +0000

    validate urls

--- server.js
+++ server.js
@@ -1,25 +1,22 @@
 var express = require('express');
 var app = express();
-var urlModule = require('url');
+var validator = require('validator');
+
 var urlsObj = {};
-var count = 0;
+var count = 1;
 var keys = [];
 
 app.use(express.static('public'));
 
-// Middleware for checking to see if url is in urlsObj
-
-// Middleware to validate url
 var setRoutes = function(req, res, next) {
-    
     for (var num in urlsObj) {
         console.log(urlsObj[num]);
         app.get('/' + num, function(req, res) {
-            res.redirect(urlsObj[num])
+            res.redirect(urlsObj[num]);
         });
     }
     next();
-}
+};
 
 app.use(setRoutes);
 
@@ -27,7 +24,7 @@ app.get('/:num', function(req, res) {
     var num = req.params.num;
     console.log(keys);
     if (keys.indexOf(num * 1) === -1) {
-        res.status('404').send({error: "No such url exists"})
+        res.status('404').send({error: "No such url exists"});
     } else {
         res.redirect(urlsObj[num]);
     }
@@ -36,28 +33,33 @@ app.get('/:num', function(req, res) {
 
 
 app.get('/new/:url(*)', function(req, res, next) {
-   var host = req.hostname;
-   var url = req.params.url;
+    var host = req.hostname;
+    var url = req.params.url;
    
- 
-   urlsObj[count] = url;
-   keys.push(count);
+    if (validator.isURL(url)) {
+        urlsObj[count] = url;
+        keys.push(count);
    
-   var short_url = req.protocol + "://" + host + '/' + count;
+        var short_url = req.protocol + "://" + host + '/' + count;
  
-   res.send({
-       original_url: url,
-       short_url: short_url,
-       obj: urlsObj
-    });
+        res.send({
+            original_url: url,
+            short_url: short_url,
+            obj: urlsObj
+        });
     
-    next();
+        next(); 
+    } else {
+        res.send({
+            error: "Invalid url passed as parameter"
+        });
+    }
 });
 
 var incrementCount = function(req, res, next) {
     count += 1;
     next();
-}
+};
 
 app.use(incrementCount);
 
